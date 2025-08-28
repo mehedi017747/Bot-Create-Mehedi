4e640face06b1b03659b3e8177ade613305b076f
mehedi017747 <mdmahadi6062@gmail.com>
2025-08-28 18:53:40 +0600

    Update baby.js

--- Script/commands/baby.js
+++ Script/commands/baby.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const simsim = "https://cyber-simsim.onrender.com";
+const simsim = "https://cyber-simsimi.onrender.com";
 
 module.exports.config = {
  name: "baby",
